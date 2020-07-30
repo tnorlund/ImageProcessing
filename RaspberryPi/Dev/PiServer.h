514b26c55c0eb08efc740f826aba9b252095396a
tnorlund <tnorlund@icloud.com>
2020-07-29 18:50:15 -0700

    First commit before implementing c++17 standards

---
+++ RaspberryPi/Dev/PiServer.h
@@ -0,0 +1,6 @@
+#ifndef __PI_SERVER__
+#define __PI_SERVER__
+
+#include <string>
+
+#endif
---
+++ RaspberryPi/Dev/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "MPU6050.h"
+#include "Socket.h"
+#include <boost/program_options.hpp>
+
+int main( int argc, char * argv[] ) {
+  std::cout << "It's working!" << std::endl;
+}
